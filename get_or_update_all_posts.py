56123e08fd54d5dfe3178fb83447e4ce3d02f9ed
miacaplan <miacaplan@gmail.com>
2016-05-24 09:15:58 +0300

    updating posts with likes and shares

--- get_or_update_all_posts.py
+++ get_or_update_all_posts.py
@@ -10,9 +10,11 @@ for page in models.pages_collection.find():
     if not page.get('username'):
         continue
     print('Updating page "{}"'.format(page['name']))
-    o = graph.get_object(page['username'] + '/posts', limit=100, order='reverse_chronological')
+    o = graph.get_object(page['username'] + '/posts', fields='id,created_time,shares,status_type',limit=100, order='reverse_chronological', summary=True)
     for post in o['data']:
         post['page_id'] = page['id']
+        post['likes'] = graph.get_object('{}/likes'.format(post['id']), summary=True)['summary']['total_count']
+        post['shares'] = post.get('shares', {}).get('count', 0)
     res = models.upsert(models.posts_collection, o['data'])
     print('Inserted {} posts, updated {} posts.'.format(res['inserted'], res['updated']))
 
